6198cf789c99b1f7bf08c50efb8d0be93c319c16
Loay alaa <40433848+xxsiders@users.noreply.github.com>
2019-05-22 02:02:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,14 +2,7 @@ import React from 'react';
 import AuthLoading from './src/screen/AuthLoading';
 import  * as firebase  from 'firebase';
 
-var config = {
-  apiKey: "AIzaSyAyVgyY7Djr86siqA5y8kOl86bL0bENheU",
-  authDomain: "pathfinder-bd467.firebaseapp.com",
-  databaseURL: "https://pathfinder-bd467.firebaseio.com",
-  projectId: "pathfinder-bd467",
-  storageBucket: "pathfinder-bd467.appspot.com",
-  messagingSenderId: "969955699963"
-};
+var config = {/* your api here */}
 firebase.initializeApp(config);
 
 export default class App extends React.Component {
